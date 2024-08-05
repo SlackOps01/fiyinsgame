a3c3b1889a3bbe7a4c2f59c89db9b0f73ae5ba50
Olanrewaju Sholarin <olanrewajusholarin5@gmail.com>
2024-08-05 09:48:02 +0100

    fixed UI

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1722846271|3098508171";
+const CACHE_VERSION = "1722847670|4497659796";
 const CACHE_PREFIX = "Fiyin-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
