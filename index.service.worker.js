d4a1f2bbcbb1a64ed5d47a28d1875439964f9dfb
Olanrewaju Sholarin <olanrewajusholarin5@gmail.com>
2024-08-06 07:13:13 +0100

    dialogue fixes

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1722924664|70051105166";
+const CACHE_VERSION = "1722924783|70170015812";
 const CACHE_PREFIX = "Fiyin-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
